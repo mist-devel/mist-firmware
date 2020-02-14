a13b0349cf33f89ad086b8b3369c42d2370f5ad9
Ramon Martinez <rampa@encomix.org>
2020-02-14 19:55:48 +0100

    Analogue joystick range scaling fix + dead zone management
    * changed min value in joystick to absolute value (as it is always negative)
    
    * dead zone management in analog joystick
    
    * code cleanup

--- usb/hid.c
+++ usb/hid.c
@@ -672,13 +672,27 @@ static void usb_process_iface (usb_hid_iface_info_t *iface,
 			if(iface->device_type == HID_DEVICE_JOYSTICK) {
 
 				for(i=0;i<2;i++) {
-					// scale to 0 -> 255 range. 99% of the joysticks already deliver that
-					if((conf->joystick_mouse.axis[i].logical.min != 0) ||
-					 (conf->joystick_mouse.axis[i].logical.max != 255)) {
-						 a[i] = ((a[i] - conf->joystick_mouse.axis[i].logical.min) * 255)/
-						 (conf->joystick_mouse.axis[i].logical.max - 
-						 conf->joystick_mouse.axis[i].logical.min);
-					}
+				        int hrange = (conf->joystick_mouse.axis[i].logical.max - abs(conf->joystick_mouse.axis[i].logical.min)) / 2;
+                                        int dead = hrange/63;
+                                        
+                                        if (a[i] < conf->joystick_mouse.axis[i].logical.min) a[i] = conf->joystick_mouse.axis[i].logical.min;
+                                          else if (a[i] > conf->joystick_mouse.axis[i].logical.max) a[i] = conf->joystick_mouse.axis[i].logical.max;
+					
+					a[i] = a[i] - (abs(conf->joystick_mouse.axis[i].logical.min) + conf->joystick_mouse.axis[i].logical.max) / 2;
+					
+					hrange -= dead;
+                                        if (a[i] < -dead) a[i] += dead;
+                                        else if (a[i] > dead) a[i] -= dead;
+                                        else a[i] = 0;
+                                        
+                                        a[i] = (a[i] * 127) / hrange;
+                                        
+                                        if (a[i] < -127) a[i] = -127;
+                                         else if (a[i] > 127) a[i] = 127;
+                                         
+                                        a[i]=a[i]+127; // mist wants a value in the range [0..255]
+
+
 				}
 
 				// handle hat if present and overwrite any axis value
