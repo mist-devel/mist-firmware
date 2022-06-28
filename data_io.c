4f43108ed5e22e27ff29a8be833ce2192295ff90
Gyorgy Szombathelyi <gyurco@freemail.hu>
2022-06-28 11:53:30 +0200

    data_io: turn on disk LED during transfer

--- data_io.c
+++ data_io.c
@@ -81,10 +81,14 @@ static void data_io_file_tx_send(FIL *file) {
       // upload directly from the SD-Card if the core supports that
       bytes2send = (file->obj.objsize + 511) & 0xfffffe00;
       file->obj.objsize = bytes2send; // hack to foul FatFs think the last block is a full sector
+      DISKLED_ON
       f_read(file, 0, bytes2send, &br);
+      DISKLED_OFF
       bytes2send = 0;
     } else {
+      DISKLED_ON
       f_read(file, sector_buffer, chunk, &br);
+      DISKLED_OFF
 
       EnableFpga();
       SPI(DIO_FILE_TX_DAT);
@@ -174,7 +178,9 @@ static void data_io_file_rx_receive(FIL *file, unsigned int len) {
 
     DisableFpga();
     bytes2receive -= chunk;
+    DISKLED_ON
     f_write(file, sector_buffer, chunk, &bw);
+    DISKLED_OFF
   }
 }
 
