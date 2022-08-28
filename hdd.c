c4de68b85c7254be8bae8bb718e59725987df4e9
Gyorgy Szombathelyi <gyurco@freemail.hu>
2022-08-28 17:13:07 +0200

    HDD: turn on disk led during CDDA play

--- hdd.c
+++ hdd.c
@@ -398,6 +398,7 @@ static void cdrom_playaudio()
     cdrom.audiostatus = AUDIO_ERROR;
     return;
   }
+  DISKLED_ON
   int offset = (cdrom.currentlba - toc.tracks[track].start) * toc.tracks[track].sector_size + toc.tracks[track].offset;
   f_lseek(&toc.file->file, offset);
   f_read(&toc.file->file, sector_buffer, 2352, &br);
@@ -410,6 +411,7 @@ static void cdrom_playaudio()
   SPI(0x00);
   spi_write(sector_buffer, 2352);
   DisableFpga();
+  DISKLED_OFF
   if (cdrom.currentlba == cdrom.endlba)
     cdrom.audiostatus = AUDIO_COMPLETE;
   else
